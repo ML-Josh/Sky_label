a018f036602bb79022b6420af2da7b4afad3da9c
ML-Josh <joshua.bj@medialand.tw>
2021-06-02 15:39:44 +0800

    Update createOrUpdateTag & Category function

--- server/app/function/category.js
+++ server/app/function/category.js
@@ -1,24 +1,41 @@
-const createOrUpdateCategories = async (categories, label, sky_id, Model) => {
+const createOrUpdateCategories = async (categories, label, sky_id, Category) => {
   if (!categories) {
-    const category = await Model.findOne({ title: 'uncategorized', sky_id });
+    const category = await Category.findOne({ title: 'uncategorized', sky_id });
     if (!category) {
-      const uncategorized = new Model({ title: 'uncategorized', sky_id });
+      const uncategorized = new Category({ title: 'uncategorized', sky_id });
       uncategorized.save();
-      label.categories.push(uncategorized._id);
+      if (label.categories.length === 0) {
+        label.categories.push(uncategorized._id);
+        uncategorized.labels.push(label._id);
+      }
+      if (label.categories.length > 0) {
+        return;
+      }
+    } else if (category && label.categories.includes(category._id)) {
+      return;
     } else {
       category.labels.push(label._id);
       category.save();
       label.categories.push(category._id);
     }
-  } else {
+  }
+  if (categories) {
     for (const c of categories) {
-      const category = await Model.findOne({ title: c, sky_id });
-      if (category) {
+      const category = await Category.findOne({ title: c, sky_id });
+      const uncategorized = await Category.findOne({ sky_id, title: 'uncategorized' });
+
+      if (label.categories.includes(uncategorized._id)) {
+        label.categories.pop();
+        await Category.updateOne({ sky_id, title: 'uncategorized' }, { $pull: { labels: label._id } });
+      }
+
+      if (category && !category.labels.includes(label._id)) {
         category.labels.push(label._id);
         category.save();
         label.categories.push(category._id);
-      } else {
-        const newCategory = new Model({ title: c, labels: label._id, sky_id });
+      }
+      if (!category) {
+        const newCategory = new Category({ title: c, labels: label._id, sky_id });
         newCategory.save();
         label.categories.push(newCategory._id);
       }
--- server/app/function/tag.js
+++ server/app/function/tag.js
@@ -1,14 +1,16 @@
-const createOrUpdateTags = async (tags, label, sky_id, Model) => {
+const createOrUpdateTags = async (tags, label, sky_id, Tag) => {
   for (const t of tags) {
-    const tag = await Model.findOne({ title: t, sky_id });
+    const tag = await Tag.findOne({ title: t, sky_id });
     if (!tag) {
-      const newTag = new Model({
+      const newTag = new Tag({
         title: t,
         sky_id,
         labels: label._id,
       });
       newTag.save();
       label.tags.push(newTag._id);
+    } else if (tag.labels.includes(label._id)) {
+      tag.save();
     } else {
       tag.labels.push(label._id);
       tag.save();
